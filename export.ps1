1894585444946847496ec14e348df88c8916c502
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2022-08-07 20:41:02 -0700

    Implement scripts to import and export db.

---
+++ export.ps1
@@ -0,0 +1,41 @@
+# Using this: https://github.com/microsoft/mssql-scripter
+# Install python and then: `pip install mssql-scripter`
+
+$server_name = "(localhost)"
+$db_name = "kodb"
+
+mssql-scripter `
+  -S $server_name `
+  -d $db_name `
+  -f $pwd/tmp/schema `
+  --file-per-object `
+  --exclude-headers `
+  --exclude-use-database `
+  --display-progress
+
+mssql-scripter `
+  -S $server_name `
+  -d $db_name `
+  -f $pwd/tmp/data `
+  --data-only `
+  --file-per-object `
+  --exclude-headers `
+  --exclude-use-database `
+  --display-progress
+
+foreach ($fn In Get-ChildItem -Path "tmp/data" -Filter "*.Table.sql") {
+  $name = $fn.Name.Split(".")[1]
+  Move-Item -Path $fn.FullName -Destination "src/data/$name.sql" -Force
+}
+
+foreach ($fn In Get-ChildItem -Path "tmp/schema" -Filter "*.Table.sql") {
+  $name = $fn.Name.Split(".")[1]
+  Move-Item -Path $fn.FullName -Destination "src/schema/$name.sql" -Force
+}
+
+foreach ($fn In Get-ChildItem -Path "tmp/schema" -Filter "*.StoredProcedure.sql") {
+  $name = $fn.Name.Split(".")[1]
+  Move-Item -Path $fn.FullName -Destination "src/procedure/$name.sql" -Force
+}
+
+Remove-Item tmp -Recurse
---
+++ import.ps1
@@ -0,0 +1,42 @@
+# Note that the script will fail if you don't have powershell sqlserver module installed.
+# To install it, run powershell as admin and execute the following command: `Install-Module sqlserver`
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+
+$server_name = "(localhost)"
+$db_name = "kodb"
+
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server($server_name)
+if (!$server) {
+  throw "Failed to connect to mssql local host server."
+  exit 1
+}
+
+if ($server.Databases[$db_name]) {
+  Write-Host 'Database already exists. Will drop and recreate it.'
+  Invoke-SqlCmd -ServerInstance $server_name -Query `
+    " `
+      BEGIN `
+        ALTER DATABASE [$db_name] SET SINGLE_USER WITH ROLLBACK IMMEDIATE; `
+        DROP DATABASE [$db_name]; `
+        CREATE DATABASE[$db_name]; `
+      END; `
+    "
+} else {
+  Write-Host "Creating database $db_name..."
+  Invoke-SqlCmd -ServerInstance $server_name -Query "CREATE DATABASE [$db_name];" -Verbose
+}
+
+foreach ($dir In $("schema", "procedure", "data", "migration")) {
+  $scripts = Get-ChildItem -Path src/$dir -Filter "*.sql"
+  Write-Host "`n`n### Running $($scripts.Count) $dir scripts... ###"
+  foreach ($fn In $scripts) {
+    Write-Host $fn.FullName
+    Invoke-Sqlcmd -ServerInstance $server_name -Database $db_name -InputFile $fn.FullName
+  }
+}
+
+Write-Host "`n`n### Creating login and user for $db_name... ###"
+Write-Host "src/misc/create_login.sql"
+Invoke-Sqlcmd -ServerInstance $server_name -Database $db_name -InputFile "src/misc/create_login.sql"
+
+cmd /c 'pause'
