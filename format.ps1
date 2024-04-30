c66079ab113c28f20684500027552c2cdd1f0856
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-04-29 23:50:45 -0400

    Introduce verbose flag to the format script.

--- format.ps1
+++ format.ps1
@@ -1,6 +1,17 @@
 # Check the .sqlfluff config file for more info
+param (
+    [bool][Parameter(Mandatory = $false)]
+    $verbose_output = $false
+)
 
-sqlfluff format `
-  "$PSScriptRoot\src\migration\" `
-  "$PSScriptRoot\src\misc\" `
+$dirs_to_format = @(
+  "$PSScriptRoot\src\migration\",
+  "$PSScriptRoot\src\misc\",
   "$PSScriptRoot\src\procedure\"
+)
+
+if ($verbose_output) {
+  sqlfluff format -vv $dirs_to_format
+} else {
+  sqlfluff format $dirs_to_format
+}
