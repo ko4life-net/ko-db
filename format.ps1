894f745fe2ee87c5561ded7dcd804ceb14af7008
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-04-29 22:17:16 -0400

    Integrate sqlfluff for tsql code formatting.

---
+++ format.ps1
@@ -0,0 +1,7 @@
+# Check the .sqlfluff config file for more info
+
+sqlfluff format `
+  "$PSScriptRoot\src\migration\" `
+  "$PSScriptRoot\src\misc\" `
+  "$PSScriptRoot\src\procedure\" `
+  "$PSScriptRoot\src\schema\"
