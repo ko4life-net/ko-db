c48407e16752acf25e572693850de995a74a2f83
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-04-30 00:28:18 -0400

    Run format on current work.

--- src/migration/10_fix_userdata_columns.sql
+++ src/migration/10_fix_userdata_columns.sql
@@ -1,13 +1,13 @@
 ALTER TABLE [dbo].[USERDATA] DROP CONSTRAINT [DF_USERDATA_strSkill]
 
 -- Implicit conversion from data type varchar to binary is not allowed.
-ALTER TABLE [dbo].[USERDATA] ADD [bSkill] BINARY (10) NULL;
+ALTER TABLE [dbo].[USERDATA] ADD [bSkill] binary(10) NULL;
 GO
-UPDATE [dbo].[USERDATA] SET [bSkill] = CONVERT(BINARY, [strSkill]);
+UPDATE [dbo].[USERDATA] SET [bSkill] = CONVERT(binary, [strSkill]);
 GO
 ALTER TABLE [dbo].[USERDATA] DROP COLUMN [strSkill];
 GO
-EXEC sp_rename '[dbo].[USERDATA].[strItem]',  'bItem', 'COLUMN';
+EXEC sp_rename '[dbo].[USERDATA].[strItem]', 'bItem', 'COLUMN';
 GO
-EXEC sp_rename '[dbo].[USERDATA].[strSerial]',  'bSerial', 'COLUMN';
+EXEC sp_rename '[dbo].[USERDATA].[strSerial]', 'bSerial', 'COLUMN';
 GO
--- src/migration/12_alter_load_char_info_procedure.sql
+++ src/migration/12_alter_load_char_info_procedure.sql
@@ -4,15 +4,23 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 ALTER PROCEDURE [dbo].[LOAD_CHAR_INFO]
-@CharId	char(21),
-@nRet	smallint OUTPUT
+  @CharId char(21),
+  @nRet smallint OUTPUT
 AS
 
 SELECT @nRet = COUNT(strUserId) FROM USERDATA WHERE strUserId = @CharId
 IF @nRet = 0
-	RETURN
+  RETURN
 
-SELECT Race, Class, HairColor, [Level], Face, Zone, bItem FROM USERDATA WHERE strUserID = @CharId
+  SELECT
+    Race,
+    [Class],
+    HairColor,
+    [Level],
+    Face,
+    Zone,
+    bItem
+  FROM USERDATA WHERE strUserID = @CharId
 
 SET @nRet = 1
 RETURN
